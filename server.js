635ef244ca34f704ef183724d5db5556e387226c
Richard Lieu <CeciliaWang@Cecilia-Ximeng-Wangs-MacBook-Pro.local>
2017-11-23 18:28:16 -0800

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var override = require("method-override");
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
